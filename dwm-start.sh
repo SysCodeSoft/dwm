b8e976a1d0627e52fa88bcb14316017902dd59d6
Max Trifonov <mp.trifonov@gmail.com>
2016-07-27 00:10:57 +0300

    toolbar keyboard master

---
+++ dwm-start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+xrdb -merge $HOME/.Xresources
+xmodmap ~/.Xmodmap
+setxkbmap -layout 'us,ru' -option 'grp:alt_shift_toggle'
+
+hash chromium-browser && chromium-browser &
+
+while true
+do
+	VOL=$(amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/')
+	LOCALTIME=$(date +%H:%M)
+	TEMP="$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))C"
+
+	if acpi -a | grep off-line > /dev/null
+	then
+		BAT="Bat. $(acpi -b | awk '{ print $4 }' | tr -d ',')"
+		xsetroot -name "$BAT $VOL $TEMP $LOCALTIME"
+	else
+		xsetroot -name "$VOL $TEMP $LOCALTIME"
+	fi
+	sleep 20s
+done &
+
+exec dwm
